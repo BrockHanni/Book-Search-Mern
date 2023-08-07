fa29643d2c1703417e11685b4f90d6a237bc0b6b
BrockHanni <hannibr26@gmail.com>
2023-08-07 17:33:33 -0600

    finialized server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schema/index');
+const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
@@ -11,24 +10,33 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+const typeDefs = `
+  type Query {
+    hello: String
+  }
+`;
 
-// Create an instance of ApolloServer
+const resolvers = {
+  Query: {
+    hello: () => "Hello, World!",
+  },
+};
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-// Integrate Apollo Server with Express
 async function startApolloServer() {
   await server.start();
   server.applyMiddleware({ app });
 }
 
 startApolloServer();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 app.use(routes);
 
