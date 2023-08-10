14e32c15c22d02a35b44ceb8bcb45cb417f70672
BrockHanni <hannibr26@gmail.com>
2023-08-10 02:44:30 -0600

    adding comments

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,68 +1,70 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
-const { signToken } = require('../utils/auth');
+// Importing necessary modules and functions
+const { AuthenticationError } = require('apollo-server-express'); // Importing Apollo Server's AuthenticationError
+const { User } = require('../models'); // Importing the User model
+const { signToken } = require('../utils/auth'); // Importing the signToken function from the auth utility
 
+// Resolvers for GraphQL queries and mutations
 const resolvers = {
-    Query: {
-        me: async (context) => {
-            // checks to see if logged in, if so, lets users select books, if not, throws error
-            if (context.user) {
-                const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
-                return userData;
-            }
-            throw new AuthenticationError('You need to be logged in!');
-        }
+  Query: {
+    me: async (context) => {
+      // Query to get the user's data if logged in
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+        return userData;
+      }
+      throw new AuthenticationError('You need to be logged in!');
     },
-    Mutation: {
-        // mutation to add a user
-        addUser: async (args) => {
-            const username = await User.create(args);
-            const token = signToken(username);
-            return { token, username };
-        },
-        // mutation to login a user
-        login: async ({ email, password }) => {
-            const username = await User.findOne({ email });
-            // gets the user 
-            const correctPassword = await username.isCorrectPassword(password);
-            const token = signToken(username);
-            // checks for correct password
-            if (!correctPassword) {
-                throw new AuthenticationError('Invalid Password');
-            }
-            // checks for correct email
-            if (!username) {
-                throw new AuthenticationError('Invalid Username');
-            }
-            return { token, username };
-        },
-        saveBook: async ({ input }, context) => {
-        
-            if (context.user) {
-                // adds book to savedBooks array, by updating the user's library
-                const updateLib = await User.findOneAndUpdate(
-                    { _id: context.user._id },
-                    { $addToSet: { savedBooks: input } }, 
-                    { new: true, runValidators: true }
-                ); 
-                return updateLib;
-            }
-            throw new AuthenticationError("To save a book, please log in!");
-        },
-       
-        removeBook: async ({ bookId }, context) => {
-            // removes book from savedBooks array, by updating the user's library
-            if (context.user) {
-                const updatedLib = await User.findOneAndUpdate(
-                    { _id: context.user._id },
-                    { $pull: { savedBooks: { bookId: bookId } } }, 
-                    { new: true, runValidators: true }
-                );
-                return updatedLib;
-            }
-            throw new AuthenticationError('To remove a book, please log in!');
-        }
-    }
-}
+  },
+  Mutation: {
+    addUser: async (args) => {
+      // Mutation to add a new user
+      const username = await User.create(args);
+      const token = signToken(username);
+      return { token, username };
+    },
+    login: async ({ email, password }) => {
+      // Mutation to log in a user
+      const username = await User.findOne({ email });
+      // Fetches the user and checks their password
+      const correctPassword = await username.isCorrectPassword(password);
+      const token = signToken(username);
+
+      // Checking for correct password
+      if (!correctPassword) {
+        throw new AuthenticationError('Invalid Password');
+      }
+      // Checking for correct email
+      if (!username) {
+        throw new AuthenticationError('Invalid Username');
+      }
+
+      return { token, username };
+    },
+    removeBook: async ({ bookId }, context) => {
+      // Mutation to remove a book from the user's savedBooks array
+      if (context.user) {
+        const updatedLib = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: bookId } } },
+          { new: true, runValidators: true }
+        );
+        return updatedLib;
+      }
+      throw new AuthenticationError('To remove a book, please log in!');
+    },
+    saveBook: async ({ input }, context) => {
+      // Mutation to save a book to the user's savedBooks array
+      if (context.user) {
+        const updateLib = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: input } },
+          { new: true, runValidators: true }
+        );
+        return updateLib;
+      }
+      throw new AuthenticationError("To save a book, please log in!");
+    },
+  },
+};
 
 module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,48 +1,52 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+// Importing required modules and packages
+const express = require('express'); // Importing the Express web framework
+const { ApolloServer } = require('apollo-server-express'); // Importing Apollo Server for GraphQL
+const path = require('path'); // Importing Node.js path module
+const db = require('./config/connection'); // Importing database connection settings
+const routes = require('./routes'); // Importing custom routes
+const app = express(); // Creating an Express application
+const PORT = process.env.PORT || 3001; // Setting the port number for the server to listen on
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+// Setting up middleware to parse incoming requests
+app.use(express.urlencoded({ extended: true })); // Allowing parsing of URL-encoded data
+app.use(express.json()); // Allowing parsing of JSON data in requests
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Function to start the Apollo Server
+async function startServer() {
+  await server.start(); // Starting the Apollo Server
+  server.applyMiddleware({ app }); // Applying the Apollo Server middleware to the Express app
+}
 
-// importing the resolvers and typeDefs
-const resolvers = {
-  Query: {
-    hello: () => "Starting the server!",
-  },
-};
-
+// GraphQL schema - type definitions (typeDefs) and resolvers
 const typeDefs = `
   type Query {
     hello: String
   }
 `;
 
-// applying the typeDefs and resolvers to the ApolloServer
+const resolvers = {
+  Query: {
+    hello: () => "Starting the server!", // Resolving the "hello" query with a static message
+  },
+};
+
+// Creating an instance of Apollo Server with defined schema and resolvers
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-// starting the ApolloServer
-async function startServer() {
-  await server.start();
-  server.applyMiddleware({ app });
-}
-
+// Starting the Apollo Server
 startServer();
 
+// Serving static files in production mode
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build'))); // Serving static files from the "build" directory
 }
 
-app.use(routes);
+app.use(routes); // Applying custom routes to the Express app
 
+// Database event listener for when the connection is established
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`)); // Starting the Express server
 });
